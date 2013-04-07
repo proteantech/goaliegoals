b1f7b7c721422e07d874241e6f18293a11ff64c3
NEXGENMAC <nexgenmac@new-host-2.home>
2013-04-06 23:44:43 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goalie::Application.config.secret_token = '4deefbe92fa4b96b83ea559edc5ba36e8412be9a453b4134117f7cb9adf44a9a32f12f611ee8c04d1e967b76ae767cda15ae7906da28c774e24ec692b3245d9a'
