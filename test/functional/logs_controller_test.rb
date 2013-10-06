d9400fa50f3bb62880b449f8d0ef14e78b182bba
mmspam31 <mmspam31@gmail.com>
2013-10-06 07:17:43 -0400

    Fixed Log Controller Functional Test

--- test/functional/logs_controller_test.rb
+++ test/functional/logs_controller_test.rb
@@ -1,49 +1,63 @@
 require 'test_helper'
 
 class LogsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   setup do
     @log = logs(:one)
+    @goal = goals(:books_2_per_month)
+    @goal.logs = [@log]
   end
 
   test "should get index" do
-    get :index
+    get :index, goal_id: @goal
     assert_response :success
     assert_not_nil assigns(:logs)
   end
 
   test "should get new" do
-    get :new
+    get :new, goal_id: @goal
     assert_response :success
   end
 
   test "should create log" do
+    @request.env["HTTP_REFERER"] = "/goals/#{@goal.id}/logs"
     assert_difference('Log.count') do
-      post :create, log: { description: @log.description, log_date: @log.log_date, quantity: @log.quantity }
+      post(:create,
+           goal_id: @goal.id,
+           log: { description: @log.description, log_date: @log.log_date,
+                  quantity: @log.quantity })
     end
 
-    assert_redirected_to log_path(assigns(:log))
+    assigns(:log)
+    assigns(:logs)
+    assigns(:goal)
+    assert_redirected_to goal_logs_url(@goal)
   end
 
   test "should show log" do
-    get :show, id: @log
+    get :show, goal_id: @goal, id: @log
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @log
+    sign_in User.first # devise
+    get :edit,  goal_id: @goal, id: @log
     assert_response :success
   end
 
   test "should update log" do
-    put :update, id: @log, log: { description: @log.description, log_date: @log.log_date, quantity: @log.quantity }
-    assert_redirected_to log_path(assigns(:log))
+    put :update, goal_id: @goal, id: @log, log: { description: @log.description, log_date: @log.log_date, quantity: @log.quantity}
+    assigns(:logs)
+    assigns(:log)
+    assert_redirected_to goal_logs_url(@goal)
   end
 
   test "should destroy log" do
     assert_difference('Log.count', -1) do
-      delete :destroy, id: @log
+      delete :destroy,  goal_id: @goal, id: @log
     end
 
-    assert_redirected_to logs_path
+    assert_redirected_to goal_logs_url(@goal)
   end
 end
