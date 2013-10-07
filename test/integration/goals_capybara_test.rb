cf9a84d9eb57cec291e1c742cd0902bc0f825976
mmspam31 <mmspam31@gmail.com>
2013-10-06 23:07:20 -0400

    Capybara Goal Create now tests all validation for required fields. Still needs to check numericality.

--- test/integration/goals_capybara_test.rb
+++ test/integration/goals_capybara_test.rb
@@ -1,6 +1,45 @@
 require 'test_helper'
 
 class GoalsCapybaraTest < ActionDispatch::IntegrationTest
+  include ActionView::Helpers::TextHelper
+
+  def setup()
+    cantBeBlank = "can't be blank"
+    isNotNumber = 'is not a number'
+
+    @fields = {
+        action: {
+            message:      cantBeBlank,
+            should_pass:  false,
+            content:      'action3'
+        },
+        quantity: {
+            message:      isNotNumber,
+            should_pass:  false,
+            content:      '333'
+        },
+        unit: {
+            message:      cantBeBlank,
+            should_pass:  false,
+            content:      'unit3'
+        },
+        frequency: {
+            message:      isNotNumber,
+            should_pass:  false,
+            content:      '3333'
+        },
+        start: {
+            message:      cantBeBlank,
+            should_pass:  false,
+            content:      '2000-12-07'
+        },
+        end: {
+            message:      cantBeBlank,
+            should_pass:  false,
+            content:      '2000-12-08'
+        }
+    }
+  end
 
   test 'create goal' do
     visit '/'
@@ -15,18 +54,103 @@ class GoalsCapybaraTest < ActionDispatch::IntegrationTest
     assert page.has_content?('Listing Goals')
     assert page.has_content?('Signed in successfully.')
 
-    fill_in 'goal_action', with: 'action1'
-    fill_in 'goal_quantity', with: '1'
-    fill_in 'goal_unit', with: 'unit1'
-    fill_in 'goal_frequency', with: '1'
-    select 'day', from: 'frequency-unit-select'
-    fill_in 'goal_start', with: '2013-10-06'
-    fill_in 'goal_end', with: '2013-10-07'
-    click_on 'Add'
+    #fill_in 'goal_action',    with: 'action1'
+    #fill_in 'goal_quantity',  with: '1'
+    #fill_in 'goal_unit',      with: 'unit1'
+    #fill_in 'goal_frequency', with: '1'
+    #select 'day',             from: 'frequency-unit-select'
+    #fill_in 'goal_start',     with: '2000-12-03'
+    #fill_in 'goal_end',       with: '2000-12-04'
+    #click_on 'Add'
+    #
+    #assert page.has_content? 'Goal was successfully created.'
+    #assert page.has_content? 'action1'
+    #assert page.has_content? 'unit1'
+    #assert page.has_content? '2000-12-03'
+    #assert page.has_content? '2000-12-04'
+    #
+    #fill_in 'goal_action',    with: 'action2'
+    #fill_in 'goal_quantity',  with: '2'
+    #fill_in 'goal_unit',      with: 'unit2'
+    #fill_in 'goal_frequency', with: '2'
+    #select 'week',            from: 'frequency-unit-select'
+    #fill_in 'goal_start',     with: '2000-12-05'
+    #fill_in 'goal_end',       with: '2000-12-06'
+    #click_on 'Add'
+    #
+    #assert page.has_content? 'Goal was successfully created.'
+    #assert page.has_content? 'action2'
+    #assert page.has_content? 'unit2'
+    #assert page.has_content? '2000-12-05'
+    #assert page.has_content? '2000-12-06'
 
-    assert page.has_content? 'Goal was successfully created.'
-    assert page.has_content? '2013-10-06'
-    assert page.has_content? '2013-10-07'
+    # Check validation with no @fields filled in
+    blank_dates_unless_should_pass
+    click_on 'Add'
+    validate_fields
+
+    # Fill in fields one at a time and ensure proper validation
+    @fields.keys.each do |k|
+      fill_next_field_and_validate(k)
+    end
 
   end
+
+  # k = key of the field to fill next
+  def fill_next_field_and_validate(k)
+    fill_next_field_and_adjust_fields_map(k)
+    click_on 'Add'
+    validate_fields
+  end
+
+  def fill_next_field_and_adjust_fields_map(k)
+    @fields[k][:should_pass] = true
+    fill_in "goal_#{k}", with: @fields[k][:content]
+    blank_dates_unless_should_pass
+  end
+
+  def blank_dates_unless_should_pass
+    unless @fields[:start][:should_pass]
+      fill_in 'goal_start', with: ''
+    end
+
+    unless @fields[:end][:should_pass]
+      fill_in 'goal_end', with: ''
+    end
+  end
+
+  def validate_fields
+
+    passes = @fields.values.collect { |mini_hash| mini_hash[:should_pass] }
+    error_count = passes.inject(0) {|err_count, should_pass| should_pass ? err_count : err_count  + 1}
+
+    if error_count > 0
+      assert page.has_content? "#{pluralize(error_count, 'error')} prohibited this goal from being saved:"
+      validate_fields_on_error
+    else
+      assert page.has_content? 'Goal was successfully created.'
+      validate_fields_on_success
+    end
+
+  end
+
+  def validate_fields_on_success
+    @fields.keys.each do |k|
+      assert page.has_content?(@fields[k][:content])
+    end
+  end
+
+  def validate_fields_on_error
+    @fields.keys.each do |k|
+      if @fields[k][:should_pass]
+        p "key=#{k}, content=#{@fields[k][:content]}"
+        assert page.has_no_content?("#{k.capitalize} #{@fields[k][:message]}")
+        assert page.has_field?("goal_#{k}", with: @fields[k][:content])
+      else
+        assert page.has_content?("#{k.capitalize} #{@fields[k][:message]}")
+        assert page.has_no_field?("goal_#{k}", with: @fields[k][:content])
+      end
+    end
+  end
+
 end
