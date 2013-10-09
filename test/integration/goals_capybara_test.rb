2f835b100baf22ff2a8fe7e394575d39dd85a6b8
mmspam31 <mmspam31@gmail.com>
2013-10-08 20:59:19 -0400

    Refactored capybara create goal, started edit goal test

--- test/integration/goals_capybara_test.rb
+++ test/integration/goals_capybara_test.rb
@@ -41,6 +41,43 @@ class GoalsCapybaraTest < ActionDispatch::IntegrationTest
     }
   end
 
+  test 'create goal' do
+
+    login()
+
+    submit_and_verify_goal1()
+    submit_and_verify_goal2()
+
+    submit_all_blank_and_validate()
+
+    # Fill in fields one at a time and ensure proper validation
+    @fields.keys.each do |k|
+      fill_next_field_and_validate(k)
+    end
+
+    submit_all_blank_and_validate()
+
+    find(:xpath, "//a[@class='edit-goal-link][0]'").click
+    assert find('.goal-submit-link').visible?
+
+  end
+
+  test 'edit goal' do
+
+    login()
+
+    submit_and_verify_goal1()
+    submit_and_verify_goal2()
+
+    submit_all_blank_and_validate()
+
+    assert !find(:xpath, "(//a[contains(@class, 'goal-submit-link')])[1]", visible: false).visible?
+    find(:xpath, "(//a[contains(@class, 'edit-goal-link')])[1]").click
+    assert find('.goal-submit-link').visible?
+
+  end
+
+
   def login
     visit '/'
     click_on 'Login'
@@ -55,17 +92,14 @@ class GoalsCapybaraTest < ActionDispatch::IntegrationTest
     assert page.has_content?('Signed in successfully.')
   end
 
-  test 'create goal' do
-
-    login()
-
-    fill_in 'goal_action',    with: 'action1'
-    fill_in 'goal_quantity',  with: '1'
-    fill_in 'goal_unit',      with: 'unit1'
+  def submit_and_verify_goal1
+    fill_in 'goal_action', with: 'action1'
+    fill_in 'goal_quantity', with: '1'
+    fill_in 'goal_unit', with: 'unit1'
     fill_in 'goal_frequency', with: '1'
-    select 'day',             from: 'frequency-unit-select'
-    fill_in 'goal_start',     with: '2000-12-03'
-    fill_in 'goal_end',       with: '2000-12-04'
+    select 'day', from: 'frequency-unit-select'
+    fill_in 'goal_start', with: '2000-12-03'
+    fill_in 'goal_end', with: '2000-12-04'
     click_on 'Add'
 
     assert page.has_content? 'Goal was successfully created.'
@@ -73,14 +107,16 @@ class GoalsCapybaraTest < ActionDispatch::IntegrationTest
     assert page.has_content? 'unit1'
     assert page.has_content? '2000-12-03'
     assert page.has_content? '2000-12-04'
+  end
 
-    fill_in 'goal_action',    with: 'action2'
-    fill_in 'goal_quantity',  with: '2'
-    fill_in 'goal_unit',      with: 'unit2'
+  def submit_and_verify_goal2
+    fill_in 'goal_action', with: 'action2'
+    fill_in 'goal_quantity', with: '2'
+    fill_in 'goal_unit', with: 'unit2'
     fill_in 'goal_frequency', with: '2'
-    select 'week',            from: 'frequency-unit-select'
-    fill_in 'goal_start',     with: '2000-12-05'
-    fill_in 'goal_end',       with: '2000-12-06'
+    select 'week', from: 'frequency-unit-select'
+    fill_in 'goal_start', with: '2000-12-05'
+    fill_in 'goal_end', with: '2000-12-06'
     click_on 'Add'
 
     assert page.has_content? 'Goal was successfully created.'
@@ -88,17 +124,13 @@ class GoalsCapybaraTest < ActionDispatch::IntegrationTest
     assert page.has_content? 'unit2'
     assert page.has_content? '2000-12-05'
     assert page.has_content? '2000-12-06'
+  end
 
-    # Check validation with no @fields filled in
+  # Check validation with no fields filled in
+  def submit_all_blank_and_validate
     blank_dates_unless_should_pass
     click_on 'Add'
     validate_fields
-
-    # Fill in fields one at a time and ensure proper validation
-    @fields.keys.each do |k|
-      fill_next_field_and_validate(k)
-    end
-
   end
 
   # k = key of the field to fill next
