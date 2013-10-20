7af81071b5005672d3a92c7b59a7e35e500fda45
michaelmilazzo <michael.milazzo@exelisinc.com>
2013-10-19 22:19:49 -0400

    Refactor capybara test to be more DRY

--- test/integration/goals_capybara_test.rb
+++ test/integration/goals_capybara_test.rb
@@ -28,6 +28,12 @@ class GoalsCapybaraTest < ActionDispatch::IntegrationTest
             should_pass:  false,
             content:      '444'
         },
+        'frequency_unit'.to_sym => {
+            message:      cantBeBlank,
+            should_pass:  true,
+            content:      '444',
+            type: :select
+        },
         start: {
             message:      cantBeBlank,
             should_pass:  false,
@@ -73,6 +79,7 @@ class GoalsCapybaraTest < ActionDispatch::IntegrationTest
       within(:xpath, "(//tr[.//a[contains(@class, 'goal-edit-link')]])[1]") do
         click_link 'goal-delete-link-id'
         a = page.driver.browser.switch_to.alert
+        assert a.text == 'Goal was successfully deleted.'
         a.accept  # can also be a.dismiss
       end
       assert page.has_content? 'Goal was successfully deleted.'
@@ -178,51 +185,61 @@ class GoalsCapybaraTest < ActionDispatch::IntegrationTest
   end
 
   def submit_and_verify_goal1
-    fill_in 'goal_action', with: 'action1'
-    fill_in 'goal_quantity', with: '1'
-    fill_in 'goal_unit', with: 'unit1'
-    fill_in 'goal_frequency', with: '1'
-    select 'day', from: 'frequency-unit-select'
-    fill_in 'goal_start', with: '2000-12-03'
-    fill_in 'goal_end', with: '2000-12-04'
+    fields = Marshal.load( Marshal.dump(@fields) )
+    fields[:action][:content] = 'action1'
+    fields[:quantity][:content] = '1'
+    fields[:unit][:content] = 'unit1'
+    fields[:frequency][:content] = '1'
+    fields[:'frequency_unit'][:content] = 'day'
+    fields[:start][:content] = '2000-12-03'
+    fields[:end][:content] = '2000-12-04'
+    fields.keys.each {|field| fields[field][:should_pass] = true}
+    fill_fields(fields)
+
     click_on 'Add'
 
-    assert page.has_content? 'Goal was successfully created.'
-    assert page.has_content? 'action1'
-    assert page.has_content? 'unit1'
-    assert page.has_content? '2000-12-03'
-    assert page.has_content? '2000-12-04'
+    validate_fields(fields)
+  end
+
+  def fill_fields(fields)
+    fields.keys.each do |field|
+      if fields[field][:type] == :select
+        select fields[field][:content], from: "#{field}-select"
+      else
+        fill_in "goal_#{field}", with: fields[field][:content]
+      end
+    end
   end
 
   def submit_and_verify_goal2
-    fill_in 'goal_action', with: 'action2'
-    fill_in 'goal_quantity', with: '2'
-    fill_in 'goal_unit', with: 'unit2'
-    fill_in 'goal_frequency', with: '2'
-    select 'week', from: 'frequency-unit-select'
-    fill_in 'goal_start', with: '2000-12-05'
-    fill_in 'goal_end', with: '2000-12-06'
+    fields = Marshal.load( Marshal.dump(@fields) )
+    fields[:action][:content] = 'action2'
+    fields[:quantity][:content] = '2'
+    fields[:unit][:content] = 'unit2'
+    fields[:frequency][:content] = '2'
+    fields[:'frequency_unit'][:content] = 'week'
+    fields[:start][:content] = '2000-12-05'
+    fields[:end][:content] = '2000-12-06'
+    fields.keys.each {|field| fields[field][:should_pass] = true}
+    fill_fields(fields)
+
     click_on 'Add'
 
-    assert page.has_content? 'Goal was successfully created.'
-    assert page.has_content? 'action2'
-    assert page.has_content? 'unit2'
-    assert page.has_content? '2000-12-05'
-    assert page.has_content? '2000-12-06'
+    validate_fields(fields)
   end
 
   # Check validation with no fields filled in
   def submit_all_blank_and_validate
     blank_dates
     click_on 'Add'
-    validate_fields
+    validate_fields(@fields)
   end
 
   # k = key of the field to fill next
   def fill_next_field_and_validate(k)
     fill_next_field_and_adjust_fields_map(k)
     click_on 'Add'
-    validate_fields
+    validate_fields(@fields)
   end
 
   def fill_next_field_and_adjust_fields_map(k)
@@ -246,36 +263,38 @@ class GoalsCapybaraTest < ActionDispatch::IntegrationTest
     fill_in 'goal_end', with: ''
   end
 
-  def validate_fields
+  def validate_fields(fields)
 
-    passes = @fields.values.collect { |mini_hash| mini_hash[:should_pass] }
+    passes = fields.values.collect { |mini_hash| mini_hash[:should_pass] }
     error_count = passes.inject(0) {|err_count, should_pass| should_pass ? err_count : err_count  + 1}
 
     if error_count > 0
       assert page.has_content? "#{pluralize(error_count, 'error')} prohibited this goal from being saved:"
-      validate_fields_on_error
+      validate_fields_on_error(fields)
     else
       assert page.has_content? 'Goal was successfully created.'
-      validate_fields_on_success
+      validate_fields_on_success(fields)
     end
 
   end
 
-  def validate_fields_on_success
-    @fields.keys.each do |k|
-      assert page.has_content?(@fields[k][:content])
+  def validate_fields_on_success(fields)
+    fields.keys.each do |k|
+      assert page.has_content?(fields[k][:content])
     end
   end
 
-  def validate_fields_on_error
-    @fields.keys.each do |k|
-      if @fields[k][:should_pass]
-        p "key=#{k}, content=#{@fields[k][:content]}"
-        assert page.has_no_content?("#{k.capitalize} #{@fields[k][:message]}")
-        assert page.has_field?("goal_#{k}", with: @fields[k][:content])
-      else
-        assert page.has_content?("#{k.capitalize} #{@fields[k][:message]}")
-        assert page.has_no_field?("goal_#{k}", with: @fields[k][:content])
+  def validate_fields_on_error(fields)
+    fields.keys.each do |k|
+      unless fields[k][:type] == :select
+        if fields[k][:should_pass]
+          p "key=#{k}, content=#{fields[k][:content]}"
+          assert page.has_no_content?("#{k.capitalize} #{fields[k][:message]}")
+          assert page.has_field?("goal_#{k}", with: fields[k][:content])
+        else
+          assert page.has_content?("#{k.capitalize} #{fields[k][:message]}")
+          assert page.has_no_field?("goal_#{k}", with: fields[k][:content])
+        end
       end
     end
   end
